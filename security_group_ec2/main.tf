ea1eb8e3c5728e0fe379eb6f1513061a83033cd4
Andrius Laukavičius <andrius@focusate.eu>
2018-09-21 17:27:18 +0300

    [ADD] security_group_ec2

---
+++ security_group_ec2/main.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "sg" {
+  name = "${var.name}"
+  description = "${var.description}"
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.ext_blocks}"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "s3" {}
+}
---
+++ security_group_ec2/variables.tf
@@ -0,0 +1,13 @@
+variable "name" {
+  description = "Name of security group"
+}
+variable "description" {
+  description = "Description of security group"
+}
+variable "vpc_id" {
+  description = "Virtual Private Cloud ID to assign"
+}
+variable "ext_blocks" {
+  description = "External IPs to allow connecting through Port 22"
+  default = "list"
+}
